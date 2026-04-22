c426970091c07f92e4c22716f4ccb54b765f3dce
le Duc Thien <ducthien09092005@gmail.com>
2026-04-22 11:02:05 +0700

    Create a basic router

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("OK")
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port: number = 8000;
+
+app.get("/topics", (req: Request, res: Response)=>{
+    res.send("Danh sách thể loại");
+});
+
+app.listen(port, () =>{
+    console.log(`Đang nghe ở cổng ${port}`);
+});
